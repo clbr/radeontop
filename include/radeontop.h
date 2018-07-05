9745967198b46cc6309f98fe99e765fa0830657c
Christian Schmidbauer <cschmidbauer@genesiscloud.com>
2018-07-05 15:12:42 +0200

    Update Copyright notice

--- include/radeontop.h
+++ include/radeontop.h
@@ -1,5 +1,6 @@
 /*
 	Copyright (C) 2012 Lauri Kasanen
+	Copyright (C) 2018 Genesis Cloud Ltd.
 
 	This program is free software: you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
